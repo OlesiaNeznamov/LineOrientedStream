79286b69a4f8a088ce2ca3d7c6f102e5a1c4039f
user <user@lenovo>
2018-05-08 23:24:31 +0300

    Commit

--- 2018-02-27_LineOrientedStream/src/PrintStreamWriterTestAppl.java
+++ 2018-02-27_LineOrientedStream/src/PrintStreamWriterTestAppl.java
@@ -5,6 +5,7 @@ public class PrintStreamWriterTestAppl {
 	private static final int N_LINES = 100;
 
 	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("tra-la-la");
 		IStreamWriter writer = new PrintWriterTest("printwriter", N_LINES);
 		writer.runTest();
 		IStreamWriter stream = new PrintStreamTest("printstream", N_LINES);
